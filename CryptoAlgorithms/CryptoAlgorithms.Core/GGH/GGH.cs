d5bc24a89c2eebae67409477482a51648a8d415e
Daniil Potemkin <daniilpot@live.com>
2021-06-03 20:19:59 +0300

    add comments to GGH;

--- CryptoAlgorithms/CryptoAlgorithms.Core/GGH/GGH.cs
+++ CryptoAlgorithms/CryptoAlgorithms.Core/GGH/GGH.cs
@@ -2,14 +2,27 @@
 
 namespace CryptoAlgorithms.Core.GGH
 {
+    /// <summary>
+    /// Схема шифрования GGH (Голдрейха-Гольдвассер-Халеви)
+    /// </summary>
     public class GGH
     {
+        /// <summary>
+        /// Генерация открытого ключа: унимодулярная матрица * базис решетки
+        /// </summary>
+        /// <param name="privateKey">Базис решетки</param>
+        /// <returns>Открытый ключ</returns>
         public Matrix GeneratePublicKey(Matrix privateKey)
         {
             var unimodular = GenerateUnimodularMatrix(privateKey.RowNumber);
             return unimodular * privateKey;
         }
 
+        /// <summary>
+        /// Генерирует унимодулярную матрицу (квадратная матрица с целыми коэффициентами, определитель которой равен +1 или -1)
+        /// </summary>
+        /// <param name="dimension">Размер</param>
+        /// <returns>Унимодулярная матрица</returns>
         private Matrix GenerateUnimodularMatrix(int dimension)
         {
             var matrix = Matrix.Identity(dimension);
@@ -31,11 +44,25 @@ namespace CryptoAlgorithms.Core.GGH
 
         }
 
+        /// <summary>
+        /// Процесс зашифрования
+        /// </summary>
+        /// <param name="message">Сообщение</param>
+        /// <param name="publicKey">Открытый ключ</param>
+        /// <param name="error">Вектор ошибки</param>
+        /// <returns>Зашифрованное сообщение</returns>
         public Vector Encrypt(Vector message, Matrix publicKey, Vector error)
         {
             return message * publicKey + error;
         }
 
+        /// <summary>
+        /// Процесс расшифрования
+        /// </summary>
+        /// <param name="message">Зашифрованное сообщение</param>
+        /// <param name="publicKey">Открытый ключ</param>
+        /// <param name="privateKey">Закрытый ключ</param>
+        /// <returns>Исходное сообщение</returns>
         public Vector Decrypt(Vector message, Matrix publicKey, Matrix privateKey)
         {
             var U = message * privateKey.Inverse();
